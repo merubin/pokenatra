713866e7d2da170a558fc32eed4d3b5150df469b
Michael Rubin <michael@rubinsworld.com>
2016-10-05 15:01:33 -0400

    MVP Done

--- app.rb
+++ app.rb
@@ -6,12 +6,33 @@ require_relative "db/connection"
 require_relative 'models/pokemon'
 
 # Show all Models
+get "/" do
+  # 1. Get all the pokemons
+  @pokemons=Pokemon.all
+  # 2. Render the pokemons
+  erb :"pokemons/index"
+end
+
 get "/pokemons" do
   # 1. Get all the pokemons
   @pokemons=Pokemon.all
   # 2. Render the pokemons
   erb :"pokemons/index"
 end
+
+# reseed database
+get "/pokemons/reseed" do
+  Pokemon.destroy_all
+  Pokemon.create(name: "Charmander", cp: rand(800), poke_type: "fire", img_url: "https://img.pokemondb.net/artwork/charmander.jpg")
+  Pokemon.create(name: "Squirtle", cp: rand(800), poke_type: "water", img_url: "https://img.pokemondb.net/artwork/squirtle.jpg")
+  Pokemon.create(name: "Pikachu", cp: rand(800), poke_type: "lightning", img_url: "https://img.pokemondb.net/artwork/pikachu.jpg")
+  Pokemon.create(name: "Bulbasaur", cp: rand(800), poke_type: "grass", img_url: "https://img.pokemondb.net/artwork/bulbasaur.jpg")
+  Pokemon.create(name: "Clefairy", cp: rand(800), poke_type: "fairy", img_url: "https://img.pokemondb.net/artwork/clefairy.jpg")
+  Pokemon.create(name: "Nidoking", cp: rand(800), poke_type: "poison ground", img_url: "https://img.pokemondb.net/artwork/nidoking.jpg")
+  Pokemon.create(name: "Gengar", cp: rand(800), poke_type: "ghost poison", img_url: "https://img.pokemondb.net/artwork/gengar.jpg")
+  Pokemon.create(name: "Blastoise", cp: rand(800), poke_type: "water", img_url: "https://img.pokemondb.net/artwork/blastoise.jpg")
+  redirect("/pokemons")
+  end
 
 # NEW Pokemon
 get '/pokemons/new' do
@@ -42,4 +63,12 @@ end
 post '/pokemons' do
   @pokemons = Pokemon.create(params[:pokemon])
   redirect("/pokemons/#{@pokemons.id}")
+end
+
+# delete Pokemon
+
+delete '/pokemons/:id' do
+  @pokemons = Pokemon.find(params[:id])
+  @pokemons.destroy
+  redirect("/pokemons")
 end
